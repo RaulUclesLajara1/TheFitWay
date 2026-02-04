c0e3ea2a34045af44c503250f3e06d6e9c002dbe
Raúl Uclés Lajara <rul2@alu.ua.es>
2026-02-04 11:08:02 +0100

    Añadido registro funcional a falta de manejo de errores en la UI

--- src/pages/registro.jsx
+++ src/pages/registro.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'; 
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { auth } from '../funciones/firebase';
+import googleLogo from "../assets/google-icon.svg"
 const Registro = () => {
     const [formData, setFormData] = useState({
-        nombre: "",
         email: "",
         password: "",
     });
@@ -18,6 +18,7 @@ const Registro = () => {
             const user = userCredential.user;
             setError("");
             setExito("Usuario creado con éxito");
+            console.log(user.uid);
         })
         .catch((error) => {
             console.log(`error: ${error.code}: ${error.message}`);
@@ -25,7 +26,27 @@ const Registro = () => {
             setError(error.message);
         })
     }    
+    const handleGoogle = () => {
+        const provider = new GoogleAuthProvider();
+        signInWithPopup(auth, provider)
+        .then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+        }).catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.customData.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            // ...
+  });
 
+    }
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
     };
@@ -47,10 +68,6 @@ const Registro = () => {
                     className="bg-zinc-950 border border-white/5 p-8 rounded-sm shadow-2xl space-y-6"
                 >
                     {/* Campos de Nombre, Email y Password  */}
-                    <div>
-                        <label className="block text-xs font-bold text-gray-500 uppercase tracking-widest mb-2 ml-1">Nombre Completo</label>
-                        <input type="text" name="nombre" value={formData.nombre} onChange={handleChange} placeholder="Ej. Iker Sánchez" className="w-full bg-black border border-white/10 px-4 py-3 text-sm text-white focus:outline-none focus:border-green-500 transition-colors" required />
-                    </div>
                     <div>
                         <label className="block text-xs font-bold text-gray-500 uppercase tracking-widest mb-2 ml-1">Correo Electrónico</label>
                         <input type="email" name="email" value={formData.email} onChange={handleChange} placeholder="atleta@thefitway.es" className="w-full bg-black border border-white/10 px-4 py-3 text-sm text-white focus:outline-none focus:border-green-500 transition-colors" required />
@@ -74,6 +91,14 @@ const Registro = () => {
                         <span className="flex-shrink mx-4 text-xs text-gray-500 uppercase">o</span>
                         <div className="flex-grow border-t border-white/10"></div>
                     </div>
+                    <button 
+                        type="button" 
+                        className="w-full flex justify-center items-center bg-white text-black font-bold py-3 px-4 rounded-sm hover:bg-gray-200 transition-all"
+                        onClick={handleGoogle}
+                        >
+                        <img src={googleLogo} alt="Google" className="w-5 h-5 mr-2 object-contain" />
+                        Registrarse con Google
+                     </button>
 
                     
                 </form>
